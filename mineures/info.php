7fccdab3f003cf4b2faee5faa5af2f9847837948
Thomas Ballini <thomasballini34130@gmail.com>
2022-06-16 14:59:54 +0200

    filtrage beau

--- mineures/info.php
+++ mineures/info.php
@@ -10,11 +10,10 @@ require '../PHP/Class.php';
         <div class="row">
             <article class="col-2" style="text-align:center;">Filtrage
                 <form method="post" action="../recherche/RechercheStage.php">
-                    <ul>
                         <tr>
-                            <div class="col-6">Entreprise</div>
+                            <div class="col-2">Entreprise</div>
                         </tr>
-                        <select id="EntrepriseNom" name="EntrepriseNom" style="width:165px;">
+                        <select id="EntrepriseNom" name="EntrepriseNom" style="width:100px;">
                             <option value="">Entreprise</option>
                             <?php
                             $NomEntreprises = new Entreprise;
@@ -26,9 +25,9 @@ require '../PHP/Class.php';
                             ?>
                         </select>
                         <tr>
-                            <div class="col-6">Compétences</label></div>
+                            <div class="col-2">Compétences</label></div>
                         </tr>
-                        <select id="StageCompetence" name="StageCompetence" style="width:165px;">
+                        <select id="StageCompetence" name="StageCompetence" style="width:100px;">
                             <option value="">Competence</option>
                             <?php
                             $OffreCompetences = new Offre;
@@ -40,10 +39,10 @@ require '../PHP/Class.php';
                             ?>
                         </select>
                         <tr>
-                            <div class="col-6">Localité</label></div>
+                            <div class="col-2">Localité</label></div>
                         </tr>
-                        <select id="StageLocalite" name="StageLocalite" style="width:165px;">
-                            <option value="">Localite</option>
+                        <select id="StageLocalite" name="StageLocalite" style="width:100px;">
+                            <option value="">Localité</option>
                             <?php
                             $OffreLocali = new Offre;
                             foreach ($OffreLocali->getOffreLocalite() as $OffreLocal) {
@@ -55,15 +54,14 @@ require '../PHP/Class.php';
                         </select>
 
                         <tr>
-                            <div class="col-6">Durée (en semaine)</label></div>
+                            <div class="col-2">Durée (semaines)</label></div>
                         </tr>
-                        <div class="col-6"><input type="number" name="StageDuree" placeholder="Durée de stage" style="width:165px;" /></div>
+                        <div class="col-6"><input type="number" name="StageDuree" placeholder="Durée de stage" style="width:90px;" /></div>
                         <br>
                         <button class="btn btn-outline-light" type="submit">Search</button>
-                    </ul>
                 </form>
             </article>
-            <article class="col-12" style="width:80%;">
+            <article class="col-9" style="width:80%;">
                 <?php
                 $users = new Offre();
                 if (isset($_GET['page']) && !empty($_GET['page'])) {
